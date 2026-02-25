2f8d8a6614eff360b891652234445d6b76028ad0
Agneta Rakstiņa <s24raksagne@vea.lv>
2026-02-25 08:30:48 +0200

    Fixed ignrore and added student mainigie

---
+++ src/model/Student.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Student {
+	//1. mainigie
+	private long studId;
+	private String name;
+	private String surname;
+	private String personCode;
+	
+	
+	//2. get funkcija
+	public long getStudId() {
+		return studId;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public String getPersonCode() {
+		return personCode;
+	}
+	
+	//3. set funkcija
+	//4.1. bezagrumenta konstruktors
+	//4.2 argumenta konstruktors
+	//5. toString fuinkcijas
+	//6. visas parejas funkcijas pec nepieciesamibas
+	
+	
+	
+	
+}
